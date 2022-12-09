950b561206ef99c3bee2b0dd6c0a994e6138f834
unknown <rillomsholeh>
2022-12-09 22:02:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Nama Saya Muhammad Sholeh</h3>
+      <h4>Ini Halaman Pertama ReactJS</h4>
+    </div>
+  )
+}
+export default App;
